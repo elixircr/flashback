9fbd628c2cfee37de5e4d4e03fb9468d836441d1
elixircr <hwarang.jang@kingsway.school.nz>
2026-03-24 11:00:46 +1300

    24.03 Sprint 2 - Moved file from old laptop to new - Added my file to github for project management and transferred my scripts over.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,13 +6,12 @@ android {
     namespace = "com.hwaranga.flashback"
     compileSdk {
         version = release(36) {
-            minorApiLevel = 1
         }
     }
 
     defaultConfig {
         applicationId = "com.hwaranga.flashback"
-        minSdk = 24
+        minSdk = 28
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -46,14 +45,10 @@ dependencies {
     implementation("androidx.camera:camera-camera2:${cameraxVersion}")
     // If you want to additionally use the CameraX Lifecycle library
     implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
-    // If you want to additionally use the CameraX VideoCapture library
-    implementation("androidx.camera:camera-video:${cameraxVersion}")
+    // connects app to hardware
+    implementation("androidx.camera:camera-camera2:${cameraxVersion}")
     // If you want to additionally use the CameraX View class
     implementation("androidx.camera:camera-view:${cameraxVersion}")
-    // If you want to additionally add CameraX ML Kit Vision Integration
-    implementation("androidx.camera:camera-mlkit-vision:${cameraxVersion}")
-    // If you want to additionally use the CameraX Extensions library
-    implementation("androidx.camera:camera-extensions:${cameraxVersion}")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/hwaranga/flashback/MainActivity.kt
+++ app/src/main/java/com/hwaranga/flashback/MainActivity.kt
@@ -1,21 +1,130 @@
 package com.hwaranga.flashback
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var previewView: PreviewView
+
+    private lateinit var cameraButton: Button
+
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var imageCapture: ImageCapture? = null
+
+    // sets constant vars
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+    }
+
+    // pretty much same as a void start func
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // sets the UI as vars using the set ID's
+        previewView = findViewById(R.id.previewView)
+        cameraButton = findViewById(R.id.cameraButton)
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        if (hasCameraPermission()) {
+            startCamera()
+        } else {
+            requestCameraPermission()
         }
+
+        // runs if button was pressed
+        cameraButton.setOnClickListener {
+            takePhoto()
+        }
+    }
+
+    private fun hasCameraPermission(): Boolean {
+        // checks if user already gave perms
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        // requests for camera perms
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startCamera()
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.surfaceProvider = previewView.surfaceProvider
+            }
+
+            imageCapture = ImageCapture.Builder().build()
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            } catch(e: Exception) {
+                Toast.makeText(this, "Camera failed: ${e.message}", Toast.LENGTH_LONG).show()
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        val photoFile = File(externalMediaDirs.firstOrNull(), "test_${System.currentTimeMillis()}.jpg")
+
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+
+        imageCapture.takePicture(outputOptions, cameraExecutor, object : ImageCapture.OnImageSavedCallback {
+            override fun onError(exception: ImageCaptureException) {
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Capture failed ${exception.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                runOnUiThread {
+                    Toast.makeText(this@MainActivity, "Saved: ${photoFile.absolutePath}", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 
 
