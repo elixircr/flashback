6b4e91185b8d2adb2a99fdac8d61c2ca3d02f70f
elixircr <hwarang.jang@kingsway.school.nz>
2026-05-23 09:56:51 +1200

    23.05 - Final changes and tweaks - added notification deleting

--- app/src/main/java/com/hwaranga/flashback/MoveScreenshotReceiver.kt
+++ app/src/main/java/com/hwaranga/flashback/MoveScreenshotReceiver.kt
@@ -1,5 +1,6 @@
 package com.hwaranga.flashback
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -9,19 +10,22 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+// broadcast receiver class; runs when the broadcast is received
 class MoveScreenshotReceiver : BroadcastReceiver() {
 
+    // function runs when broadcast received
+    // copys screenshotted photo over to apps gallery
     override fun onReceive(context: Context, intent: Intent) {
         val filePath = intent.getStringExtra("file_path") ?: return
         val sourceFile = File(filePath)
         if (!sourceFile.exists()) return
 
-        val sdf = SimpleDateFormat("ddMMyy_HHmmss", Locale.getDefault())
-        val dateString = sdf.format(Date())
+        val dateString = SimpleDateFormat("ddMMyy_HHmmss", Locale.getDefault()).format(Date())
         val destFile = File(context.filesDir, "flashback_${dateString}_1d.jpg")
 
         sourceFile.copyTo(destFile, overwrite = true)
-        sourceFile.delete()
-        Log.d("Flashback", "Screenshot moved to: ${destFile.absolutePath}")
+
+        val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(2)
     }
 }
