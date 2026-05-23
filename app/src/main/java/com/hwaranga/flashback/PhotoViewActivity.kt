7940366c4085c2cbcd7acedb4cd638a15129f047
elixircr <hwarang.jang@kingsway.school.nz>
2026-05-23 12:11:28 +1200

    23.05 - Final changes and tweaks - merge not done properly??

--- app/src/main/java/com/hwaranga/flashback/PhotoViewActivity.kt
+++ app/src/main/java/com/hwaranga/flashback/PhotoViewActivity.kt
@@ -1,10 +1,12 @@
 package com.hwaranga.flashback
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import java.io.File
 
 class PhotoViewActivity : AppCompatActivity() {
 
@@ -17,6 +19,13 @@ class PhotoViewActivity : AppCompatActivity() {
 
         val imageView = findViewById<ImageView>(R.id.fullImageView)
         val backButton = findViewById<ImageButton>(R.id.backButton)
+
+        val deleteButton = findViewById<Button>(R.id.deleteButton)
+
+        deleteButton.setOnClickListener {
+            File(photoPath).delete()
+            finish()
+        }
 
         Glide.with(this)
             .load(photoPath)
